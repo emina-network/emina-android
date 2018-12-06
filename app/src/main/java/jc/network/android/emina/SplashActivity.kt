d3283fbda43e3fafd754562135164a7aed12e4d6
Jay C <jayc.chowdhary@gmail.com>
2018-12-06 16:07:20 -0500

    Initial commit.

---
+++ app/src/main/java/jc/network/android/emina/SplashActivity.kt
@@ -0,0 +1,12 @@
+package jc.network.android.emina
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
